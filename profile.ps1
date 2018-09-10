cb0c2781af6d8719bfa66769be9f989b3ae44144
Michael Woosley <doc@digiofficeconcepts.org>
2018-09-10 00:29:28 -0400

    Add files via upload
    Powershell AllUsersCurrentHost profile file

---
+++ profile.ps1
@@ -0,0 +1,11 @@
+# Display greeting
+
+Clear-host
+Write-host "`t `t `t----------------ATTENTION----------------"
+Write-host "`t `t `t This computer is for use by authorized personnel only"
+Write-host " "
+Write-host " "
+
+# Set alias for restarting local computer
+
+set-alias -name rmc -value restart-computer
